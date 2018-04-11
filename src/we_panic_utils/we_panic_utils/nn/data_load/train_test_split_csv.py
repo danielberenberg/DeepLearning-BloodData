ccd997ff398d72f62621ede36533c5771c033562
danielberenberg <djberenb@uvm.edu>
2018-04-10 22:03:27 -0400

    some frame stuff

--- src/we_panic_utils/we_panic_utils/nn/data_load/train_test_split_csv.py
+++ src/we_panic_utils/we_panic_utils/nn/data_load/train_test_split_csv.py
@@ -4,13 +4,18 @@ import we_panic_utils.basic_utils.basics as base
 import csv
 import os
 import sys
+import pandas as pd
+import random
+
 """
 Public API
 """
 
+
 def data_set_from_csv(csv_path, augmented_dir=None):
     ignore = False
-    if not augmented_dir is None:
+
+    if augmented_dir is not None:
         ignore = True
 
     if not os.path.exists(csv_path):
@@ -26,6 +31,7 @@ def data_set_from_csv(csv_path, augmented_dir=None):
                 data[path] = (int(hr), int(rr))
     return data
 
+
 def data_set_to_csv(data_set, csv_path, verbose=True):
     if not csv_path.endswith('.csv'):
         csv_path += '.csv'
@@ -40,6 +46,130 @@ def data_set_to_csv(data_set, csv_path, verbose=True):
 
             # writer.writerow([key, data[0], data[1]])
             writer.writerow([key, str(data[0]), str(data[1])])
+
+
+def generate_paths2labels(df, data_path):
+    """
+    create the sought after and beloved `paths2labels`
+    also know as `filtered_#$@%ing_paths` from a dataframe
+    
+    format assuming "S%4d/Trial%d_frames" is the name of every
+    directory inside of data_path
+
+    args:
+        df : DataFrame
+        data_path : path to data
+    """
+    
+    filtered_paths = dict()
+   
+    subj_fmt = "S%04d"
+    trial_fmt = "Trial%d_frames"
+
+    for row in df.iterrows():
+        subject, trial, h_rate, resp_rate = row['Subject'], row['Trial'], row['Heart Rate'], row['Respiratory Rate']
+    
+        subject = subj_fmt % subject
+        trial = trial_fmt % trial
+    
+        P = os.path.join(data_path, subject, trial)
+        filtered_paths[P] = (h_rate, resp_rate)
+
+    return filtered_paths
+
+
+def ttswcsv2(data_path, metadata, output_dir,
+             test_split=0.2, val_split=0.1,
+             csvnames={'train': 'train.csv', 'val': 'val.csv', 'test': 'test.csv'}, verbose=True):
+    """
+    Train Test Split With Csv Support Version 2
+    perform a split on metadata into separate sets, ensure that no 1 subject
+    spans across 2 sets
+    
+    the idea for this function is to use it on any frame directory and corresponding
+    metadata csv and so the usage for the combination of directories would be to 
+    take the union of the output here
+    
+    the way the `ignore_augmented` functionality is implemented in this function
+    is to simply provide 'set_name' in csvnames to be None, or simply exclude 'set_name' from
+    csvnames entirely
+
+    eg
+    --
+    suppose the data_path passed in is the augmented set of frames and we do not want to test on this set.
+    the function signature is then:
+
+        ttswcsv2(<augmented_path>, 
+                 <metadata>, 
+                 <output_dir>, 
+                 test_split=<$>, 
+                 val_split=<$>,
+                 csvnames = {'train':<train_name>, 'val':<val_name>'})
+
+    args:
+        data_path : str - path to frame directory
+        metadata : the subject, trial, hrate, resprate csv
+        output_dir : the output directory
+        test_split : percentage of full data set approximately the test set should be
+        val_split : percentage of the train set approximately to allocate for validation
+        csvnames : the names of the output csvs, relative to the output_dir
+    """
+    
+    if type(csvnames) is not dict:
+        raise ValueError("got a bum csvname parameter of type %s, should be a dict" % str(type(csvnames))) 
+   
+    if 'train' not in csvnames:
+        csvnames['train'] = None
+
+    if 'val' not in csvnames:
+        csvnames['val'] = None
+
+    if 'test' not in csvnames:
+        csvnames['test'] = None
+
+    metadf = pd.read_csv(metadata)
+    instances = metadf['Subject'] 
+    subjects = list(set(instances))
+    
+    random.shuffle(subjects)
+    
+    filtered_training_paths, filtered_testing_paths, filtered_validation_paths = None, None, None
+    
+    N = len(subjects)
+    
+    if csvnames['test'] is not None:
+        test = int(test_split * N)
+        test_samples = subjects[:test]
+        subjects = subjects[test:]
+        
+        test_df = metadf[metadf['Subject'].isin(test_samples)]
+        test_df.to_csv(os.path.join(output_dir, csvnames['test']))
+        
+        filtered_testing_paths = generate_paths2labels(test_df, data_path)
+
+    random.shuffle(subjects)
+    
+    N = len(subjects)
+    
+    if csvnames['val'] is not None:
+        val = int(val_split * N)
+        val_samples = subjects[:val] 
+        subjects = subjects[val:]
+        
+        val_df = metadf[metadf['Subject'].isin(val_samples)]
+        val_df.to_csv(os.path.join(output_dir, csvnames['val']))
+        
+        filtered_validation_paths = generate_paths2labels(val_df, data_path)
+
+    if csvnames['train'] is not None:
+        train_samples = subjects
+        train_df = metadf[metadf['Subject'].isin(train_samples)]
+        train_df.to_csv(os.path.join(output_dir, csvnames['train']))
+
+        filtered_training_paths = generate_paths2labels(train_df, data_path)
+
+    return filtered_training_paths, filtered_testing_paths, filtered_validation_paths 
+
 
 
 def train_test_split_with_csv_support(regular_data_path, filtered_csv, consolidated_csv, dir_out, 
@@ -106,7 +236,7 @@ def train_test_split_with_csv_support(regular_data_path, filtered_csv, consolida
     all_paths = util.fetch_paths_with_labels(consolidated_csv, regular_data_path)
 
     filtered_training_paths = util.filter_path_with_set(training_set, all_paths, augmented_data_path, verbose)
-    filtered_testing_paths = util.filter_path_with_set(testing_set, all_paths, augmented_data_path, verbose)
+    filtered_testing_paths = util.filter_path_with_gtset(testing_set, all_paths, augmented_data_path, verbose)
     filtered_validation_paths = util.filter_path_with_set(validation_set, all_paths, augmented_data_path, verbose)
    
     base.check_exists_create_if_not(dir_out, not verbose)
